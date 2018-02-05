4735c36e09454bd34467d8ea71f39fda0292a35b
Andy Dam <andy@andydam.com>
2018-02-05 15:58:54 -0800

    feat: change to only use body-parser on routes that need it

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const router = require('./server/routes');
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
-server.use(bodyParser.json());
 server.use(router);
 server.listen(PORT, () => console.log(`nozama is listening on port ${PORT}`));
 
