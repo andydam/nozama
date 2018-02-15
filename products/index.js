be21ae843f4a646388483df717a854df56485fbc
Andy Dam <andy@andydam.com>
2018-02-14 16:56:21 -0800

    Fix/integration (#14)
    * fix: typo using one instead of two services

--- products/index.js
+++ products/index.js
@@ -21,7 +21,7 @@ router.get('/products/details/:id', async (req, res) => {
     if (req.session.passport) {
       analytics.details
         .view(req.session.passport.user, req.params.id, new Date())
-        .catch(err => console.log('error saving analytics, ', err));
+        .catch((err) => console.log('error saving analytics, ', err));
     }
   } catch (err) {
     res.status(500).json(err);
@@ -36,7 +36,7 @@ router.get('/products/search/:query', async (req, res) => {
     if (req.session.passport) {
       analytics.search
         .query(req.session.passport.user, req.params.query, new Date())
-        .catch(err => console.log('error saving analytics, ', err));
+        .catch((err) => console.log('error saving analytics, ', err));
     }
   } catch (err) {
     res.status(500).json(err);
@@ -45,13 +45,22 @@ router.get('/products/search/:query', async (req, res) => {
 
 router.get('/products/search/:query/:page', async (req, res) => {
   try {
-    const results = await cache.memoize(search.byString, req.params.query, Number(req.params.page));
+    const results = await cache.memoize(
+      search.byString,
+      req.params.query,
+      Number(req.params.page),
+    );
     res.status(200).json(results);
 
     if (req.session.passport) {
       analytics.search
-        .queryPage(req.session.passport.user, req.params.query, req.params.page, new Date())
-        .catch(err => console.log('error saving analytics, ', err));
+        .queryPage(
+          req.session.passport.user,
+          req.params.query,
+          req.params.page,
+          new Date(),
+        )
+        .catch((err) => console.log('error saving analytics, ', err));
     }
   } catch (err) {
     res.status(500).json(err);
@@ -60,7 +69,10 @@ router.get('/products/search/:query/:page', async (req, res) => {
 
 router.get('/products/reviews/:id', async (req, res) => {
   try {
-    const reviewsList = await cache.memoize(reviews.getByProductId, req.params.id);
+    const reviewsList = await cache.memoize(
+      reviews.getByProductId,
+      req.params.id,
+    );
     res.status(200).json(reviewsList);
   } catch (err) {
     res.status(500).json(err);
@@ -72,7 +84,10 @@ router.get('/products/reviews', async (req, res) => {
     return res.sendStatus(401);
   }
   try {
-    const reviewsList = await cache.memoize(reviews.getByUserId, req.session.passport.user);
+    const reviewsList = await cache.memoize(
+      reviews.getByUserId,
+      req.session.passport.user,
+    );
     return res.status(200).json(reviewsList);
   } catch (err) {
     return res.status(500).json(err);
@@ -93,7 +108,7 @@ router.post('/products/reviews/:id', jsonParser, async (req, res) => {
 
     analytics.reviews
       .post(req.session.passport.user, req.params.id, new Date())
-      .then(null, err => console.log('error saving analytics, ', err));
+      .then(null, (err) => console.log('error saving analytics, ', err));
     return res.status(200).json(reviewId);
   } catch (err) {
     return res.status(500).json(err);
@@ -105,21 +120,30 @@ router.get('/products/related/:id', async (req, res) => {
     return res.sendStatus(504);
   }
   try {
-    const urls = [`${contentPath}${req.params.id}`, `${contentPath}${req.params.id}`];
-    const data = await Promise.all(urls.map(url =>
-      fetch(url).then(resp =>
-        resp.text().then((text) => {
-          try {
-            const json = JSON.parse(text);
-            return json;
-          } catch (err) {
-            return [];
-          }
-        }))));
+    const urls = [
+      `${collaborativePath}${req.params.id}`,
+      `${contentPath}${req.params.id}`,
+    ];
+    const data = await Promise.all(
+      urls.map((url) =>
+        fetch(url)
+          .then((resp) =>
+            resp.text().then((text) => {
+              try {
+                const json = JSON.parse(text);
+                return json;
+              } catch (err) {
+                return [];
+              }
+            }),
+          )
+          .catch(() => []),
+      ),
+    );
 
     return res.status(200).json({
       collaborative: data[0],
-      content: data[1].length ? data[1].related : data[1],
+      content: Array.isArray(data[1]) ? data[1] : data[1].related,
     });
   } catch (err) {
     return res.status(500).json(err);
